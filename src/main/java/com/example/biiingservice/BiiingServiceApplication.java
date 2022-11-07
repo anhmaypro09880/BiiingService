52d88d18d20f37d1f5bdf15deb4412cfcbf7b9f8
Phan Nguyen Tan Trong <anhmaypro09880@gmail.com>
2022-11-07 12:52:56 +0700

    finish billing service

--- src/main/java/com/example/biiingservice/BiiingServiceApplication.java
+++ src/main/java/com/example/biiingservice/BiiingServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BiiingServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BiiingServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BiiingServiceApplication.class, args);
+    }
 
 }
