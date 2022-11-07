5e2c9735ad2293ed99d4db5ba6b9799ecf3027a5
Phan Nguyen Tan Trong <anhmaypro09880@gmail.com>
2022-11-07 12:51:04 +0700

    finish billing service

---
+++ src/main/java/com/example/biiingservice/BiiingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.biiingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiiingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiiingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/biiingservice/BiiingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biiingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiiingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
